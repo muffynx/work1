4136a059c3e2a62fe43f2fd79320dac4d01447bd
muffyn_pp <72650070+muffynx@users.noreply.github.com>
2022-11-28 16:59:45 +0700

    addme

---
+++ test.cs
@@ -0,0 +1,27 @@
+using AVG5STUDENT;
+
+Student student1 = new Student();
+student1.name = "Natdanai Pakpoom";
+student1.weight = 50.5f;
+
+Student student2 = new Student();
+student2.name = "Witchapon Yuenyong";
+student2.weight = 70.0f;
+
+Student student3 = new Student();
+student3.name = "Jaruvit Sangkaewsirikul";
+student3.weight = 85.5f;
+
+Student student4 = new Student();
+student4.name = "Juntima Promwang";
+student4.weight = 46.5f;
+
+Student student5 = new Student();
+student5.name = "Poommipat Wongpan";
+student5.weight = 75.5f;
+
+float avgWeight = (student1.weight + student2.weight + student3.weight + student4.weight + student5.weight) / 5;
+Console.WriteLine("Weight avg of student: " + avgWeight);
+Console.WriteLine(student1.name + student1.weight);
+Console.WriteLine(student4.name + student4.weight);
+Console.WriteLine(student1.name + " 653450088-3");
